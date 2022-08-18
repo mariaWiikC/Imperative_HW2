2defaca959d10c9833609642ad5c4a69e51e7207
Wiik Candido Maria <wiikcandido.maria@syk.fi>
2022-08-18 18:24:30 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int a = 1;
+        int b = 2;
+        int result;
+
+        if (a == b)
+            System.out.println( result = (a+b) * (a+b) );
+        else
+            System.out.println(result = a + b);
+
+    }
+}
